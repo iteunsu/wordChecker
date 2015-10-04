e439d34c98b5bbc350267b7b3169d3f455b9425f
iteunsu <eunsuyee@gmail.com>
2015-10-04 21:54:46 +0900

    Initial Commit

---
+++ WordChecker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WordChecker
+//
+//  Created by esyee on 2015. 10. 4..
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
